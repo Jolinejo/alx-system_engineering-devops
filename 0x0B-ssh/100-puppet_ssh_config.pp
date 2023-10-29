77d7eaef24963aee833c4f20cbf73a305a82b393
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-10-29 11:53:42 -0700

    task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,5 @@
 #configure ssh
 # Create file
-file_line { 'line':
-  path => '~/.ssh/config',
-  line => 'Host 54.236.47.113',
-}
 file_line { 'line2':
   path => '~/.ssh/config',
   line => 'PasswordAuthentication no',

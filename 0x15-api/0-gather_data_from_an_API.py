281d4b5b3d56bd81f314591cf0293bcd2882ffcd
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-11-27 11:33:21 -0800

    task

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""module to fetch URL"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    num = sys.argv[1]
+    api_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(num)
+    response = requests.get(api_url)
+    dicti = response.json()
+    titles = []
+    tasks = 0
+    for todo in dicti:
+            tasks = tasks + 1
+            if todo["completed"] == True:
+                titles.append(todo["title"])
+    api_url = "https://jsonplaceholder.typicode.com/users/{}".format(num)
+    response = requests.get(api_url)
+    name = response.json()["name"]
+    tot = len(titles)
+    print("Employee {} is done with tasks({}/{}):".format(name, tot, tasks))
+    for item in titles:
+        print("\t {}".format(item))

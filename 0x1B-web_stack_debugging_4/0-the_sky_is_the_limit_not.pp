b87bd527bc06c4458cd28ce3e40399ce413b1ebe
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-12-21 15:49:47 -0800

    task

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 #handle many requests
 exec {'fix_ulimit':
   provider => shell,
-  command  => 'sed -i "s/15/4096/" /etc/default/nginx',
+  command  => 'sudo sed -i "s/15/4096/" /etc/default/nginx',
   path     => '/bin/:/usr/local/bin/'
 }
 
 exec {'restart_nginx':
   provider => shell,
-  command  => 'service nginx restart',
+  command  => 'sudo service nginx restart',
   path     => '/bin/:/usr/local/bin/'
 }
